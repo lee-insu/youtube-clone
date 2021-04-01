9e9ed3b1c64ede658fc73c3101e85e33de66ca1a
lee-insu <carpediem7760@gmail.com>
2021-04-01 23:30:56 +0900

    test

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 
 class Nav extends Component {
+
+    formRef = React.createRef();
+    submitRef = React.createRef();
+
+    onsubmitEvent = (e) => {
+        e.preventDefault();
+        const search = submitRef.current.value
+        console.log(search); 
+    }
+
     render() {
         return (
             <div>
               <nav> youtube</nav>
               <form ref={formRef} onSubmit={onSubmitEvent}>
-              <input type="text" name="" id=""/>    
+              <input ref={submitRef} type="text" name="" id=""/>    
+              <button className="find-button">find</button>
               </form>   
             </div>
         );
